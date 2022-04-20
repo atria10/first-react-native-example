56b85eece49e524304b1822b2dc39f3964b2e030
atria10 <mario.atria@stevejobs.academy>
2022-04-20 19:26:21 +0200

    created a simple app in react native with a simple template. Just to undertand how it works

--- App.tsx
+++ App.tsx
@@ -4,17 +4,72 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <View style={styles.nav}></View>
+      <View style={styles.main}>
+        <View style={styles.sidebar}></View>
+        <View style={styles.center}>
+          <View style={styles.redBox}></View>
+        </View>
+        <View style={styles.content}>
+          <View style={styles.greenBox}></View>
+          <View style={styles.blueBox}></View>
+          <View style={styles.greenBox}></View>
+        </View>
+      </View>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
+  blueBox: {
+    backgroundColor: 'blue',
+    height: 50,
+     width: 100,
+  },
+  center: {
+    alignItems: 'center',
+    display: 'flex',
+    justifyContent: 'center',
+    height:'100%',
+    position: 'relative',
+  },
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    display: 'flex',
   },
+  content: {
+    alignItems: 'center',
+    display: 'flex',
+    justifyContent: 'space-evenly',
+    height: '100%',
+    width: '60%',
+  },
+  greenBox: {
+    width: 100,
+    height: 200,
+    backgroundColor: 'green',
+  },
+  main: {
+    alignItems: 'flex-start',
+    display: 'flex',
+    flexDirection: 'row',
+    position: 'relative',
+    height: '90vh',
+  },
+  nav: {
+    backgroundColor: 'cyan',
+    height: '10vh',
+    width: '100%'
+  },
+  redBox: {
+    backgroundColor: 'red',
+    height: 40,
+    left:-10,
+    width: 20,
+  },
+  sidebar: {
+    backgroundColor: 'gray',
+    height: '100%',
+    width: '40%',
+  }
 });
